dff9f4e79f5c609e9c5721d8aef847df4db828cb
winebarrel <sugawara@winebarrel.jp>
2023-11-14 11:08:29 +0900

    Add test

--- Makefile
+++ Makefile
@@ -9,6 +9,14 @@ build:
 vet:
 	go vet ./...
 
+.PHONY: test
+test:
+	go test -v -count=1 ./...
+
+.PHONY: testacc
+testacc:
+	$(MAKE) test TEST_ACC=1
+
 .PHONY: lint
 lint:
 	golangci-lint run
---
+++ task_test.go
@@ -0,0 +1,66 @@
+package qube_test
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/winebarrel/qube"
+)
+
+func Test_Tasc_Acc(t *testing.T) {
+	if !testAcc {
+		t.Skip()
+	}
+
+	assert := assert.New(t)
+	require := require.New(t)
+
+	tt := []struct {
+		URL    string
+		Driver qube.DBDriver
+	}{
+		{URL: testMySQLURL, Driver: qube.DBDriverMySQL},
+		{URL: testPostgreSQLURL, Driver: qube.DBDriverPostgreSQL},
+	}
+
+	for _, t := range tt {
+		f, _ := os.CreateTemp("", "")
+		defer os.Remove(f.Name())
+		f.WriteString(`{"q":"select 1"}` + "\n") //nolint:errcheck
+		f.Sync()                                 //nolint:errcheck
+
+		task := &qube.Task{
+			Options: &qube.Options{
+				AgentOptions: qube.AgentOptions{
+					Force: false,
+				},
+				DataOptions: qube.DataOptions{
+					DataFile:   f.Name(),
+					Key:        "q",
+					Loop:       true,
+					Random:     false,
+					CommitRate: 0,
+				},
+				DBConfig: qube.DBConfig{
+					DSN:    t.URL,
+					Driver: t.Driver,
+					Noop:   false,
+				},
+				Nagents:  1,
+				Rate:     0,
+				Time:     1 * time.Second,
+				Progress: false,
+			},
+			ID: testUUID,
+		}
+
+		report, err := task.Run()
+
+		require.NoError(err)
+		assert.Equal(testUUID, report.ID)
+		assert.NotEqual(0, report.AvgQPS)
+	}
+}

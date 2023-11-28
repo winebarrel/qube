ece3e4f7b17e9fdc8dc244ecd9823862f3fe2344
winebarrel <sugawara@winebarrel.jp>
2023-11-28 16:57:27 +0900

    chore: Refactor Test_Progress

--- progress_test.go
+++ progress_test.go
@@ -2,7 +2,6 @@ package qube_test
 
 import (
 	"context"
-	"strings"
 	"testing"
 	"time"
 
@@ -33,5 +32,5 @@ func Test_Progress(t *testing.T) {
 	buf := make([]byte, 1024)
 	_, err = ptmx.Read(buf)
 	require.NoError(err)
-	assert.Equal("00:01 | 0 agents / exec 0 queries, 0 errors (0 qps)", strings.Trim(string(buf), "\r\x00"))
+	assert.Contains(string(buf), "00:01 | 0 agents / exec 0 queries, 0 errors (0 qps)")
 }

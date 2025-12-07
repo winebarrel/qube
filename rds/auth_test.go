509b1012ad9573900c2846dfec5d91f40bd3cea6
Genki Sugawara <sugawara@winebarrel.jp>
2025-12-07 22:14:59 +0900

    Update rds/auth_test.go

--- rds/auth_test.go
+++ rds/auth_test.go
@@ -17,11 +17,6 @@ func TestResolveCNAME(t *testing.T) {
 
 	t.Run("resolve CNAME", func(t *testing.T) {
 		host, err := rds.ResolveCNAME("www.google.com")
-
-		if err != nil {
-			t.Skipf("skipping test due to network error: %v", err)
-		}
-
 		require.NoError(t, err)
 		assert.NotEmpty(t, host)
 	})
